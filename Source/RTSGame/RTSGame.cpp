100034b946b881a6a7b2d14968be389f378e3f97
JoeyManso <jmanso522@gmail.com>
2017-02-12 19:28:23 -0500

    Initial RTSGame Commit

---
+++ Source/RTSGame/RTSGame.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "RTSGame.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RTSGame, "RTSGame" );
+
+DEFINE_LOG_CATEGORY(LogRTSGame)
